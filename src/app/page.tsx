41b8ae262340fd3fda7c5a0b2fbe7ec1072d0154
bunmigrey <bunmigrey@icloud.com>
2024-02-08 11:41:48 +0100

    heropage commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="main">
         <div className='main--subcontainer'>
           <h1>Next-Gen <span>Innovations</span> Forged Here</h1>
-          <p>Step into the future with ATAfrica – where next-gen ideas meet boundless possibilities. Join us on the entrepreneurial journey of a lifetime, shaping tomorrow's success stories, one innovation at a time. Welcome to the next era of creativity and collaboration!</p>
+          <p>Step into the future with ATAfrica, where next-gen ideas meet boundless possibilities. Join us on the entrepreneurial journey of a lifetime, shaping tomorrow's success stories, one innovation at a time. Welcome to the next era of creativity and collaboration!</p>
           <div>
             <p>PARTNER WITH US</p>
             <Image src={arrow} alt='arrow pointed right' />
