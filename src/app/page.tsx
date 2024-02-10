0bc8c43de560d119366977c4224fac076ba7ebb3
bunmigrey <bunmigrey@icloud.com>
2024-02-10 07:20:22 +0100

    two more sections done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,8 +111,6 @@ export default function Home() {
         <p>PORTFOLIO</p>
         <p>Featured Startups</p>
         <div>
-          {/* <Image src={mockup1} alt='ft9ja mockup' className='ft9ja--mockup1' />
-          <Image src={mockup2} alt='ft9ja mockup details' className='ft9ja--mockup2' /> */}
           <div className='ft9ja--mockup1' ><Image src={mockup1} alt='ft9ja mockup' className='ft9ja--mockup1img' /></div>
           <div className='ft9ja--mockup2'><Image src={mockup2} alt='ft9ja mockup details' className='ft9ja--mockup2img' /> </div>
         </div>
