08420f3fa75bf6035fb57c70e16fe21379566a16
bunmigrey <bunmigrey@icloud.com>
2024-02-11 01:55:26 +0100

    landing page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,10 @@ import serviceLogo2 from '../../assets/services_logo2.svg';
 import serviceLogo3 from '../../assets/services_logo3.svg';
 import mockup1 from '../../assets/Ft9ja--mockup.svg';
 import mockup2 from '../../assets/ft9ja--mockup--details.svg';
+import insta from '../../assets/insta--icon.svg';
+import twitter from '../../assets/twitter--icon.svg';
+import facebook from '../../assets/facebook--icon.svg';
+
 
 
 
@@ -141,14 +145,38 @@ export default function Home() {
       </div>
 
       <footer className='footer'>
-          
+        <div className='footer--div1'>
+          <p>Get our newsletters</p>
+          <p>Subscribe to get curated newsletters and insights</p>
+          <input placeholder="Email address" type='text' className='email' />
+          <div className='subscribe'>
+            <p>Subscribe</p>
+            <Image src={arrow} alt='arrow pointed right' className='arrrow--icon' />
+          </div>
+        </div>
+        <hr/>
+        <div className='footer--div2'>
+          <p>who we are</p>
+          <p>Portfolio</p>
+          <p>Career</p>
+          <p>Contact</p>
+        </div>
+        <hr/>
+        <div className='footer--div3'>
+          <p>&copy; Asokoro Technologies Limited, all rights reserved</p>
+          <p>Office Address:30 Oladimeji Alo Street, Lekki Phase one, Lagos</p>
+          <p>Company email: atafrica@yahoo.com</p>
+          <p>Company number: 081055885569</p>
+          <div className='footer--div4'>
+            <p>Privacy Policy</p>
+            <div>
+            <Image src={insta} alt='instagram' className='insta' />
+            <Image src={twitter} alt='twitter icon' className='twitter' />
+            <Image src={facebook} alt='facebook icon' className='facebook' />
+            </div>
+          </div>
+        </div>
       </footer>
     </main>
   )
 }
-
-
-
-
-{/* <Image src={mockup1} alt='ft9ja mockup' className='ft9ja--mockup1' />
-          <Image src={mockup2} alt='ft9ja mockup details' className='ft9ja--mockup2' /> */}
