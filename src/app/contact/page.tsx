0252bef61150846975dc7228789f22c535e4dafd
bunmigrey <bunmigrey@icloud.com>
2024-02-16 15:40:28 +0100

    careers page correction done2

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import "./contact.scss";
 
 
-export default function Contact () {
-    return(
+export default function Contact() {
+    return (
         <main>
             <div className="main">
-
+                <div className="main--sub1">
+                    <p>Get in touch!</p>
+                    <p>We&apos;d love to hear from you. Our team will reach out ASAP!</p>
+                </div>
             </div>
         </main>
     )
