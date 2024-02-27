363335846c54581e34e930892dcb0d7e13b31a44
bunmigrey <bunmigrey@icloud.com>
2024-02-27 16:14:50 +0100

    about page annimation done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,14 @@
+'use client'
+
 import Image from "next/image";
 import './about.scss';
-import aboutCulture1 from "../../../assets/about--culture1.png";
-import aboutCulture2 from "../../../assets/about--culture2.png";
-import aboutCulture3 from "../../../assets/about--culture3.png";
+import { useEffect } from "react";
+// import aboutCulture1 from "../../../assets/about--culture1.png";
+// import aboutCulture2 from "../../../assets/about--culture2.png";
+// import aboutCulture3 from "../../../assets/about--culture3.png";
+import aboutCulture4 from "../../../assets/aboutculture4.png";
+import aboutCulture5 from "../../../assets/aboutculture5.png";
+import aboutCulture6 from "../../../assets/aboutculture6.png";
 import linkedinIcon from "../../../assets/linkedin--icon.svg";
 import person1 from "../../../assets/person1.jpg";
 import person2 from "../../../assets/person2.jpg";
@@ -12,28 +18,92 @@ import person5 from "../../../assets/person5.jpg";
 import person6 from "../../../assets/person6.jpg";
 import Prefooter from "../style/components/prefooterHomepage/prefooterHomepage";
 import Footer from "../style/components/footer/footer";
+import { useMediaQuery } from 'react-responsive';
 
 
 
 export default function About() {
+    const isLargeScreen = useMediaQuery({ query: '(min-width: 1300px)' });
+
+
+    useEffect(() => {
+        let cultureMediaDiv = document.querySelectorAll(".culture--media--div");
+        let culturePhotoDiv = document.querySelectorAll(".culture--photo--div");
+        let cultureMainDiv = document.querySelector(".about--culturepage--media");
+
+
+        if (isLargeScreen) {
+            cultureMediaDiv.forEach((culturemediadiv) => {
+                culturemediadiv.addEventListener("mouseenter", () => {
+                    culturemediadiv.childNodes[0].style.transition = "height .5s";
+                    culturemediadiv.style.transition = "display .5s"
+
+                    console.log(isLargeScreen)
+
+                    setTimeout(() => {
+                        culturemediadiv.childNodes[0].style.height = '400px';
+                        cultureMainDiv.style.flexWrap = "nowrap";
+                    }, 100);
+
+                    //for elements in the nodes without the mouse event 
+                    cultureMediaDiv.forEach((element) => {
+                        if (element !== culturemediadiv) {
+                            element.childNodes[0].style.width = '240px';
+                            element.childNodes[0].style.transition = 'width .5s';
+
+                            setTimeout(() => {
+                                element.childNodes[0].style.transition = 'width .5s';
+                            }, 100);
+                        }
+                    });
+
+                });
+
+                culturemediadiv.addEventListener("mouseleave", () => {
+
+                    culturemediadiv.childNodes[0].style.transition = "height .5s";
+                    culturemediadiv.style.transition = "display .5s";
+
+                    setTimeout(() => {
+                        culturemediadiv.childNodes[0].style.height = '241px';
+                        cultureMainDiv.style.flexWrap = "wrap";
+                    }, 100);
+
+                    //for elements in the nodes without the mouse event 
+                    cultureMediaDiv.forEach((element) => {
+                        if (element !== culturemediadiv) {
+                            element.style.transition = 'width .5s';
+
+                            setTimeout(() => {
+                                element.childNodes[0].style.width = '400px';
+                            }, 100);
+                        }
+                    });
+                });
+
+            })
+        }
+    }, [])
+
+
 
 
     const Culture = [
         {
             id: 1,
-            image: aboutCulture1,
+            image: aboutCulture4,
             title: "Innovation Hub",
             body: "Where creativity drives every data."
         },
         {
             id: 2,
-            image: aboutCulture3,
+            image: aboutCulture5,
             title: "Agile Mindset",
             body: "Adapting swiftly, succeeding together."
         },
         {
             id: 3,
-            image: aboutCulture2,
+            image: aboutCulture6,
             title: "Passion for Excellence",
             body: "Striving for the extraordinary, everyday."
         }
@@ -108,7 +178,7 @@ export default function About() {
                     {
                         Culture.map((culture) => (
                             <div className="culture--media--div" key={culture.id}>
-                                <Image src={culture.image} className="culture--photo" alt="about--culture" />
+                                <div className="culture--photo--div"><Image src={culture.image} style={{ objectFit: "cover", objectPosition: "center 0%" }} className="culture--photo" alt="about--culture" /></div>
                                 <div>
                                     <p>{culture.title}</p>
                                     <p>{culture.body}</p>
@@ -125,7 +195,7 @@ export default function About() {
                     {
                         Teams.map((team) => (
                             <div key={team.id}>
-                                <Image src={team.image} width='240' height='240' style={{ borderRadius: '50%', objectFit: "cover", objectPosition: "center 0%" }} />
+                                <Image src={team.image} className="team--photos" alt="team photos" width='240' height='240' style={{ borderRadius: '50%', objectFit: "cover", objectPosition: "center 0%" }} />
                                 <p>{team.name}</p>
                                 <p>Founder & CEO, ATAfrica</p>
                                 <Image src={linkedinIcon} alt="linkedin--icon" />
